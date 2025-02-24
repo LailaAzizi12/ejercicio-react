d1e5f3dae0d7d68e6c1f1453f33c5334df4300af
Laila <azizi.122844@e.unavarra.es>
2025-02-24 20:36:10 +0100

    Añado bootstrap e iconos para input A y B y los cuatro botones.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Button,
+  Col,
+  Container,
+  FloatingLabel,
+  Form,
+  Row,
+} from "react-bootstrap";
+import "./App.css";
+import { PlusCircleDotted } from "react-bootstrap-icons";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Row>
+          <Col>
+            <FloatingLabel controlId="floatingInput" label="A" className="mb-3">
+              <Form.Control type="number" placeholder="name@example.com" />
+            </FloatingLabel>
+          </Col>
+          <Col>
+            <FloatingLabel controlId="floatingPassword" label="B">
+              <Form.Control type="number" placeholder="Password" />
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="outline-info">+</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-warning">-</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-primary">*</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-secondary">/</Button>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
